ab2c1f942653c5d365e3e8bc6636b6e8426d22f1
Luciano <luciano.murruni@gmail.com>
2023-02-26 09:43:39 +0100

    feat: add tailwindcss (#4)

---
+++ tailwind.config.js
@@ -0,0 +1,64 @@
+const plugin = require("tailwindcss/plugin");
+const colors = require("tailwindcss/colors");
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: (theme) => ({
+        patternTop: "url('../images/patternTop.svg')",
+        patternBottom: "url('../images/patternBottom.svg')"
+      }),
+      boxShadow: {
+        DEFAULT: "rgba(103, 151, 255, 0.11) 0px 8px 60px 0px,  rgba(103, 151, 255, 0.11) 0px 12px 90px 0px;",
+        monotone: "0 20px 25px -5px rgba(0,0,0,0.1),0 10px 10px -5px rgba(0,0,0,0.04)"
+      },
+
+      colors: {
+        black: "#16171b"
+      },
+      fontFamily: {
+        serif: ['"Playfair Display"', "serif"],
+        sans: [
+          '"Inter"',
+          "system-ui",
+          "-apple-system",
+          "BlinkMacSystemFont",
+          '"Segoe UI"',
+          "Roboto",
+          '"Helvetica Neue"',
+          "Arial",
+          '"Noto Sans"',
+          "sans-serif",
+          '"Apple Color Emoji"',
+          '"Segoe UI Emoji"',
+          '"Segoe UI Symbol"'
+        ] // Ensure fonts with spaces have " " surrounding it.
+      }
+    }
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: [
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/line-clamp"),
+    require("@tailwindcss/aspect-ratio"),
+    plugin(function ({ addVariant, e, postcss }) {
+      addVariant("firefox", ({ container, separator }) => {
+        const isFirefoxRule = postcss.atRule({
+          name: "-moz-document",
+          params: "url-prefix()"
+        });
+        isFirefoxRule.append(container.nodes);
+        container.append(isFirefoxRule);
+        isFirefoxRule.walkRules((rule) => {
+          rule.selector = `.${e(`firefox${separator}${rule.selector.slice(1)}`)}`;
+        });
+      });
+    })
+  ]
+};
