c5307de99e7c79b406edc83b54fe330d2abb8540
Luciano Murruni <luciano.murruni@gmail.com>
2023-02-27 17:49:39 +0100

    feat: add toggle button for mobile

--- src/app/components/layout/header/header.component.ts
+++ src/app/components/layout/header/header.component.ts
@@ -24,28 +24,28 @@ import { NAV_MENU, BLACK_BUTTON, WHITE_BUTTON } from './constants';
           </a>
           <button
             class="lg:hidden focus:outline-none"
-            click="open = !open"
+            (click)="toggleNav()"
             aria-label="hidden"
           >
             <svg fill="#000000" viewBox="0 0 20 20" class="w-6 h-6">
               <path
-                x-show="!open"
+                *ngIf="!showNav"
                 fill-rule="evenodd"
                 d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z"
                 clip-rule="evenodd"
               ></path>
               <path
-                x-show="open"
+                *ngIf="showNav"
                 fill-rule="evenodd"
                 d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
                 clip-rule="evenodd"
-                style="display: none"
+                style="display: block"
               ></path>
             </svg>
           </button>
         </div>
         <nav
-          :class="{'flex': open, 'hidden': !open}"
+          [ngClass]="{'flex': showNav, 'hidden': !showNav}"
           class="flex-col items-end flex-grow hidden px-5 md:pb-0 lg:flex lg:justify-end lg:flex-row lg:space-x-4"
         >
           <a *ngFor="let item of NavMenu"
@@ -64,4 +64,11 @@ export class HeaderComponent {
   public NavMenu = NAV_MENU;
   public WhiteButton = WHITE_BUTTON;
   public BlackButton = BLACK_BUTTON;
+  public showNav = false;
+
+  toggleNav(){
+    this.showNav = !this.showNav;
+    console.log(this.showNav);
+  }
+
 }
