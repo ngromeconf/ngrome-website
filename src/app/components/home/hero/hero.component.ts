602a200afe8faf9f702d5b34b2c6c9fdb5905f01
Luciano <luciano.murruni@gmail.com>
2023-03-04 20:32:15 +0100

    feat: fix header (#28)
    * feat: fix header
     - button color
     - open/close service
    
    * fix: hero margin section
    
    * feat: add scroll event
    
     - close menu
     - enhancement: optimize event only for mobile
     - add animation on open/close
    
    * feat: add opacity for mobile device

--- src/app/components/home/hero/hero.component.ts
+++ src/app/components/home/hero/hero.component.ts
@@ -6,15 +6,15 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   template: `
-    <section class="relative w-full h-screen overflow-hidden bg-center bg-no-repeat bg-cover bg-patternTop">
+    <section class="relative w-full h-screen overflow-hidden bg-no-repeat bg-cover bg-patternTop">
       <div class="container relative w-full px-8 pt-32 pb-24 mx-auto lg:px-4">
         <div class="flex flex-col w-full mb-12 text-left lg:text-center items-center ">
-          <strong class="mb-4 text-xs font-semibold tracking-widest uppercase text-slate-500">only at</strong>
-          <div class="lg:flex">
+          <strong class="mb-12 text-xs font-semibold tracking-widest uppercase text-slate-500">only at</strong>
+          <div class="lg:flex mb-12">
           <img src="./ngrome-mmxxiii.svg">
           </div>
-          <p class="mx-auto text-lg leading-snug text-slate-500 lg:w-1/2">
-            The largest Italian Angular Conference
+          <p class="mx-auto text-lg leading-snug text-slate-500 lg:w-1/2 bg-white bg-opacity-25">
+            Whether you are a beginner, intermediate, or an expert, this will be the best opportunity to learn about the latest trends in the Angular world and web technologies.
           </p>
         </div>
         <div class="flex w-full mt-6  justify-center ">
@@ -23,7 +23,7 @@ import { CommonModule } from '@angular/common';
               href="https://wicked-templates.gumroad.com/l/monotone-starter"
               class="inline-flex items-center px-8 py-3 text-lg text-white transition-all duration-500 ease-in-out transform bg-red-500 border-2 rounded-lg md:mb-2 lg:mb-0 hover:border-white hover:bg-green-500 focus:ring-2 ring-offset-current ring-offset-2"
             >
-              Buy for $29
+              Buy for $110
             </a>
           </div>
         </div>
