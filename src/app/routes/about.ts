6d543435d0ae05c7b1b225f9472662c3663d0f5a
Luciano Murruni <luciano.murruni@gmail.com>
2023-03-27 21:09:42 +0200

    feat: add footer to pages

--- src/app/routes/about.ts
+++ src/app/routes/about.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../components/layout/header/header.component";
+import { FooterComponent } from "../components/layout/footer/footer.component";
 
 @Component({
     selector: 'app-about',
     standalone: true,
     template: `
     <app-header />
+
     
+
+    <app-footer />
   `,
-    imports: [HeaderComponent]
+    imports: [HeaderComponent, FooterComponent]
 })
 export default class AboutComponent {
 
