db842fab52621050728732e8bfc776e91686fca5
Luciano Murruni <luciano.murruni@gmail.com>
2023-03-27 14:49:06 +0200

    feat: fix some details

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,18 +1,18 @@
-import { Component } from "@angular/core";
-import { HeaderComponent } from "../components/layout/header/header.component";
-import { HeroComponent } from "../components/home/hero/hero.component";
-import { FeaturesComponent } from "../components/home/features/features.component";
-import { VenueComponent } from "../components/home/venue/venue.component";
+import { Component } from '@angular/core';
+import { HeaderComponent } from '../components/layout/header/header.component';
+import { HeroComponent } from '../components/home/hero/hero.component';
+import { FeaturesComponent } from '../components/home/features/features.component';
+import { VenueComponent } from '../components/home/venue/venue.component';
 
 @Component({
-    selector: "app-home",
-    standalone: true,
-    template: `
+  selector: 'app-home',
+  standalone: true,
+  template: `
     <app-header />
     <app-hero />
     <app-features />
     <app-venue />
   `,
-    imports: [HeaderComponent, HeroComponent, FeaturesComponent, VenueComponent]
+  imports: [HeaderComponent, HeroComponent, FeaturesComponent, VenueComponent],
 })
 export default class HomeComponent {}
