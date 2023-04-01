a724a9e57b7e0076909f858136c33178cea870e2
GMalacaria <63113360+GMalacaria@users.noreply.github.com>
2023-04-01 15:17:07 +0200

    fix: display footer in page past editions (#47)
    * fix: dispay footer in page past editions
    and remove padding bottom footer
    
    * fix: missing class in section

--- src/app/routes/past-editions.ts
+++ src/app/routes/past-editions.ts
@@ -1,22 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from '../components/layout/header/header.component';
-import { FooterComponent } from "../components/layout/footer/footer.component";
+import { FooterComponent } from '../components/layout/footer/footer.component';
 
 @Component({
-    selector: 'app-past-editions',
-    standalone: true,
-    template: `
-
+  selector: 'app-past-editions',
+  standalone: true,
+  template: `
     <section
-      class="relative w-full h-screen bg-center bg-no-repeat bg-cover bg-patternTop"
+      class="relative w-full h-screen bg-center bg-no-repeat bg-cover bg-patternTop d-contents"
     >
-      
-        <router-outlet></router-outlet>
-      
+      <router-outlet></router-outlet>
     </section>
-
   `,
-    imports: [RouterOutlet, HeaderComponent, FooterComponent]
+  styles: [
+    `
+      .d-contents {
+        display: contents;
+      }
+    `,
+  ],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
 })
 export default class PastEditionsComponent {}
