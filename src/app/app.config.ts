3b57e887c97e42615df05c9d4ec2c31ac3d05c3d
Luciano <luciano.murruni@gmail.com>
2023-04-23 09:02:26 +0200

    Packages upgrade (#56)
    * add first config
    
    * some small update to packages
    
    * feat: upgrade angular & analog packages

---
+++ src/app/app.config.ts
@@ -0,0 +1,12 @@
+import { ApplicationConfig } from '@angular/core';
+import { provideClientHydration } from '@angular/platform-browser';
+import { provideHttpClient } from '@angular/common/http';
+import { provideFileRouter } from '@analogjs/router';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideFileRouter(),
+    provideHttpClient(),
+    provideClientHydration(),
+  ],
+};
