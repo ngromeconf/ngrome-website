7582718db2013242d8b7001f0c19c9e2a0fa6a73
Luciano <luciano.murruni@gmail.com>
2023-04-24 12:01:29 +0200

    fix: build with correct vite config (#57)

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,7 @@
 import 'zone.js';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideFileRouter } from '@analogjs/router';
 
 import { AppComponent } from './app/app.component';
-import { FooterComponent } from './app/components/layout/footer/footer.component';
+import { appConfig } from './app/app.config';
 
-bootstrapApplication(AppComponent, {
-  providers: [provideFileRouter()],
-});
-
-bootstrapApplication(FooterComponent, {
-  providers: [provideFileRouter()],
-});
+bootstrapApplication(AppComponent, appConfig);
