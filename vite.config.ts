574c3c6af4bf616dc7cf20cd6435fb5d52a5b526
Luciano Murruni <luciano.murruni@gmail.com>
2023-08-29 17:11:07 +0200

    add ssg

--- vite.config.ts
+++ vite.config.ts
@@ -15,6 +15,7 @@ export default defineConfig(({ mode }) => ({
   plugins: [
     analog({
       ssr: true,
+      static: true,
       prerender: {
         routes: async () => ['/', '/about', '/sponsors'],
         sitemap: {
