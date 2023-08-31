f3a1c47c2362fb95c1becd2315773d53cab61c35
Luciano Murruni <luciano.murruni@gmail.com>
2023-08-31 11:37:21 +0200

    feat: update images in home

--- src/app/components/layout/footer/footer.component.ts
+++ src/app/components/layout/footer/footer.component.ts
@@ -15,8 +15,8 @@ import { FOOTER_MENU } from './constants';
             <span class="text-lg font-bold text-white lg:text-2xl"
               ><div class="inline-flex items-center">
                 <svg
-                  width=""
-                  height=""
+                  width="220"
+                  height="40"
                   viewBox="0 0 809 147"
                   fill="none"
                   xmlns="http://www.w3.org/2000/svg"
