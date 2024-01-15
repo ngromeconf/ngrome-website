b1c439c8b3807b4b8ab29ec7b28b5ae483b8b40a
Giovanni.Malacaria <giovanni.malacaria@conte.it>
2024-01-15 17:53:30 +0100

    fix: change link img Giuseppe

--- src/app/components/about/team/team.model.ts
+++ src/app/components/about/team/team.model.ts
@@ -1,18 +1,68 @@
 export interface Contributor {
-    name: string,
-    imageUrl: string,
-    linkedIn?: string,
-    twitter?: string,
-    gitHub?: string
+  name: string;
+  imageUrl: string;
+  linkedIn?: string;
+  twitter?: string;
+  gitHub?: string;
 }
 
 export const CONTRIBUTORS: Contributor[] = [
-    { name: 'Ermanno Battista', gitHub: 'https://github.com/ermannob', imageUrl: 'https://media.licdn.com/dms/image/D4D03AQHByIjqO7WFLA/profile-displayphoto-shrink_800_800/0/1671222927859?e=1710374400&v=beta&t=tTQNiBKdRzxVpctiWAgsT5MTs_eNVr-UoG31FIT9WlI', linkedIn: 'https://www.linkedin.com/in/ermannobattista/', twitter: 'https://twitter.com/BattistaErmanno' },
-    { name: 'Luciano Murruni', gitHub: 'https://x.com/MrMurruni?t=rh8waZXkiQWel9l_dUUZbw&s=09', twitter: 'https://x.com/MrMurruni?t=rh8waZXkiQWel9l_dUUZbw&s=09', imageUrl: 'https://media.licdn.com/dms/image/D4E03AQEbq-ezO-e4ew/profile-displayphoto-shrink_800_800/0/1702370312167?e=1710374400&v=beta&t=dMuV9AuyznMnDnlG_ePVemPAK49_9y1E-UKHLkufM2s', linkedIn: 'https://www.linkedin.com/in/lucianomurruni/' },
-    { name: 'Marco Matascioli', imageUrl: 'https://media.licdn.com/dms/image/C5603AQH07lWkSb0Eqg/profile-displayphoto-shrink_800_800/0/1516623474319?e=1710374400&v=beta&t=EWIz7epVDwtWhRd0ZDx2hoAk7yE7DbBRLjKoZJasuFs', linkedIn: 'https://www.linkedin.com/in/marco-matascioli-84928a36/?originalSubdomain=it', twitter: 'https://twitter.com/marcomascioli' },
-    { name: 'Raul Valentini', imageUrl: 'https://media.licdn.com/dms/image/C4D03AQFnd7CuyLDIdA/profile-displayphoto-shrink_800_800/0/1613153631508?e=1710374400&v=beta&t=BJf3aXoJD9Gs-bU7Lch_3pCdX4XvwwJTmo_hFyJRCYg', linkedIn: 'https://www.linkedin.com/in/raul-valentini/', twitter: 'https://twitter.com/RaulValentino', gitHub: 'https://github.com/raulvalentini/' },
-    { name: 'Giuseppe Ettorre', imageUrl: 'src/assets/photo/contributors/giuseppe-img.jpg', linkedIn: 'https://www.linkedin.com/in/gettorre/', gitHub: 'https://github.com/peppoasap' },
-    { name: 'Valentina Ricci', linkedIn: 'https://www.linkedin.com/in/valentina-ricci/', imageUrl: 'https://media.licdn.com/dms/image/C4D03AQGqK2x_7Fvn7g/profile-displayphoto-shrink_800_800/0/1651228806454?e=1710374400&v=beta&t=ojC2FYRS6uWObR917sk5ijMWS6qbOvfsr9NH5RhEUdI', gitHub: 'https://github.com/Brittu2016' },
-    { name: 'Claudia Biondi', imageUrl: 'https://media.licdn.com/dms/image/D4E03AQFD_mjH2u1BMw/profile-displayphoto-shrink_800_800/0/1687970725970?e=1710374400&v=beta&t=nJMw1Sxm6plSX3jCMeHHeMx7OJbVma9whWgivKYpLUk', linkedIn: 'https://www.linkedin.com/in/claudia-biondi-650b39192/' },
-    { name: 'Giovanni Malacaria', imageUrl: 'https://media.licdn.com/dms/image/C4D03AQF0je4wic_hSA/profile-displayphoto-shrink_800_800/0/1581940624187?e=1710374400&v=beta&t=Awoscu5atIY5WClz9icHLh9DIm1kTvyxaqJDwDMrUP0', linkedIn: 'https://www.linkedin.com/in/giovanni-malacaria-599390129/', gitHub: 'https://github.com/GMalacaria' }
-]
+  {
+    name: 'Ermanno Battista',
+    gitHub: 'https://github.com/ermannob',
+    imageUrl:
+      'https://media.licdn.com/dms/image/D4D03AQHByIjqO7WFLA/profile-displayphoto-shrink_800_800/0/1671222927859?e=1710374400&v=beta&t=tTQNiBKdRzxVpctiWAgsT5MTs_eNVr-UoG31FIT9WlI',
+    linkedIn: 'https://www.linkedin.com/in/ermannobattista/',
+    twitter: 'https://twitter.com/BattistaErmanno',
+  },
+  {
+    name: 'Luciano Murruni',
+    gitHub: 'https://x.com/MrMurruni?t=rh8waZXkiQWel9l_dUUZbw&s=09',
+    twitter: 'https://x.com/MrMurruni?t=rh8waZXkiQWel9l_dUUZbw&s=09',
+    imageUrl:
+      'https://media.licdn.com/dms/image/D4E03AQEbq-ezO-e4ew/profile-displayphoto-shrink_800_800/0/1702370312167?e=1710374400&v=beta&t=dMuV9AuyznMnDnlG_ePVemPAK49_9y1E-UKHLkufM2s',
+    linkedIn: 'https://www.linkedin.com/in/lucianomurruni/',
+  },
+  {
+    name: 'Marco Matascioli',
+    imageUrl:
+      'https://media.licdn.com/dms/image/C5603AQH07lWkSb0Eqg/profile-displayphoto-shrink_800_800/0/1516623474319?e=1710374400&v=beta&t=EWIz7epVDwtWhRd0ZDx2hoAk7yE7DbBRLjKoZJasuFs',
+    linkedIn:
+      'https://www.linkedin.com/in/marco-matascioli-84928a36/?originalSubdomain=it',
+    twitter: 'https://twitter.com/marcomascioli',
+  },
+  {
+    name: 'Raul Valentini',
+    imageUrl:
+      'https://media.licdn.com/dms/image/C4D03AQFnd7CuyLDIdA/profile-displayphoto-shrink_800_800/0/1613153631508?e=1710374400&v=beta&t=BJf3aXoJD9Gs-bU7Lch_3pCdX4XvwwJTmo_hFyJRCYg',
+    linkedIn: 'https://www.linkedin.com/in/raul-valentini/',
+    twitter: 'https://twitter.com/RaulValentino',
+    gitHub: 'https://github.com/raulvalentini/',
+  },
+  {
+    name: 'Giuseppe Ettorre',
+    imageUrl: '/photo/contributors/giuseppe-img.jpg',
+    linkedIn: 'https://www.linkedin.com/in/gettorre/',
+    gitHub: 'https://github.com/peppoasap',
+  },
+  {
+    name: 'Valentina Ricci',
+    linkedIn: 'https://www.linkedin.com/in/valentina-ricci/',
+    imageUrl:
+      'https://media.licdn.com/dms/image/C4D03AQGqK2x_7Fvn7g/profile-displayphoto-shrink_800_800/0/1651228806454?e=1710374400&v=beta&t=ojC2FYRS6uWObR917sk5ijMWS6qbOvfsr9NH5RhEUdI',
+    gitHub: 'https://github.com/Brittu2016',
+  },
+  {
+    name: 'Claudia Biondi',
+    imageUrl:
+      'https://media.licdn.com/dms/image/D4E03AQFD_mjH2u1BMw/profile-displayphoto-shrink_800_800/0/1687970725970?e=1710374400&v=beta&t=nJMw1Sxm6plSX3jCMeHHeMx7OJbVma9whWgivKYpLUk',
+    linkedIn: 'https://www.linkedin.com/in/claudia-biondi-650b39192/',
+  },
+  {
+    name: 'Giovanni Malacaria',
+    imageUrl:
+      'https://media.licdn.com/dms/image/C4D03AQF0je4wic_hSA/profile-displayphoto-shrink_800_800/0/1581940624187?e=1710374400&v=beta&t=Awoscu5atIY5WClz9icHLh9DIm1kTvyxaqJDwDMrUP0',
+    linkedIn: 'https://www.linkedin.com/in/giovanni-malacaria-599390129/',
+    gitHub: 'https://github.com/GMalacaria',
+  },
+];
