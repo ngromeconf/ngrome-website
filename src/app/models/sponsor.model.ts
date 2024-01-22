565cf5273c988c457d7abc9fad258a39db75e90f
Giuseppe <ettorre19@gmail.com>
2024-01-22 10:17:50 +0100

    92 Community Partners Section ( Sponsor Page ) (#102)
    * feat: added community partners to sponsors section
    
    * fix: changed angular love logo
    
    ---------
    
    Co-authored-by: Luciano <luciano.murruni@gmail.com>

--- src/app/models/sponsor.model.ts
+++ src/app/models/sponsor.model.ts
@@ -1,25 +1,25 @@
 export interface SponsorInterface {
-    name: string;
-    image: string;
-    url: string;
-  }
-  
+  name: string;
+  image: string;
+  url: string;
+}
+
 export enum sponsorType {
-  Main = 'main', 
-  Gold = 'gold', 
-  Silver = 'silver', 
-  Bronze = 'bronze', 
-  Diversity = 'diversity', 
-  technical = 'technical'
+  Main = 'main',
+  Gold = 'gold',
+  Silver = 'silver',
+  Bronze = 'bronze',
+  Diversity = 'diversity',
+  technical = 'technical',
+  Community = 'community',
 }
 
 export type Sponsors = {
-  
   Main?: SponsorInterface[];
   Gold?: SponsorInterface[];
   Silver?: SponsorInterface[];
   Bronze?: SponsorInterface[];
   Diversity?: SponsorInterface[];
   technical?: SponsorInterface[];
-
+  Community?: SponsorInterface[];
 };
