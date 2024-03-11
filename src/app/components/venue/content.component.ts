f3de85bfa2206ed2ebafd7b4fe9411566272dc55
Luciano <luciano.murruni@gmail.com>
2024-03-11 17:04:36 +0000

    feat:  update venue and image

--- src/app/components/venue/content.component.ts
+++ src/app/components/venue/content.component.ts
@@ -32,10 +32,60 @@ import { Component } from '@angular/core';
           </h1>
           <div class="mx-auto text-lg">
           <blockquote class="mt-8 text-xl leading-8 text-slate-500 text-left">
-            <span class="font-serif text-6xl text-black"> L</span>Located near Via Aurelia, a few kilometers from Fiumicino Airport, the hotel offers easy access to the center of Rome and its many attractions, including historical monuments, museums and shopping areas.
+            <span class="font-serif text-6xl text-black"> L</span>ocated near Via Aurelia, a few kilometers from Fiumicino Airport, the hotel offers easy access to the center of Rome and its many attractions, including historical monuments, museums and shopping areas.
           </blockquote>
         </div>
         <br />
+
+       
+      </div>
+    </div>
+    <div class="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-4 lg:py-20">
+
+    <div class="grid gap-4 sm:grid-cols-2 lg:grid-cols-3 ">
+      <div>
+        <div class="relative overflow-hidden transition duration-300 transform rounded shadow-lg lg:hover:-translate-y-2 hover:shadow-2xl">
+          <img class="object-cover w-full h-56 md:h-64 xl:h-80" src="https://res.cloudinary.com/dp1gwjz5w/image/upload/c_crop,ar_3:4/v1710175743/venue/Hotel-Midas---059--Hotel-Midas-Roma_cm5o3e.jpg" alt="Person" />
+          <div class="absolute inset-0 flex flex-col justify-center px-5 py-4 text-center transition-opacity duration-300 bg-black bg-opacity-75 opacity-0 hover:opacity-100">
+            <p class="mb-1 text-lg font-bold text-gray-100">Oliver Aguilerra</p>
+            <p class="mb-4 text-xs text-gray-100">Product Manager</p>
+            <p class="mb-4 text-xs tracking-wide text-gray-400">
+              Vincent Van Gogh’s most popular painting, The Starry Night.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div>
+        <div class="relative overflow-hidden transition duration-300 transform rounded shadow-lg lg:hover:-translate-y-2 hover:shadow-2xl">
+          <img class="object-cover w-full h-56 md:h-64 xl:h-80" src="https://res.cloudinary.com/dp1gwjz5w/image/upload/c_crop,ar_3:4/v1710175720/venue/Salone-Midas--11---Hotel-Midas-Roma_smjz5h.jpg" alt="Person" />
+          <div class="absolute inset-0 flex flex-col justify-center px-5 py-4 text-center transition-opacity duration-300 bg-black bg-opacity-75 opacity-0 hover:opacity-100">
+            <p class="mb-1 text-lg font-bold text-gray-100">Marta Clermont</p>
+            <p class="mb-4 text-xs text-gray-100">Design Team Lead</p>
+            <p class="mb-4 text-xs tracking-wide text-gray-400">
+              Amet I love liquorice jujubes pudding croissant I love pudding.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div>
+        <div class="relative overflow-hidden transition duration-300 transform rounded shadow-lg lg:hover:-translate-y-2 hover:shadow-2xl">
+          <img class="object-cover w-full h-56 md:h-64 xl:h-80" src="https://res.cloudinary.com/dp1gwjz5w/image/upload/c_crop,ar_4:3/v1709832170/venue/kouclpg2l8bkgbsbzkul.jpg" alt="Person" />
+          <div class="absolute inset-0 flex flex-col justify-center px-5 py-4 text-center transition-opacity duration-300 bg-black bg-opacity-75 opacity-0 hover:opacity-100">
+            <p class="mb-1 text-lg font-bold text-gray-100">Anthony Geek</p>
+            <p class="mb-4 text-xs text-gray-100">CTO, Lorem Inc.</p>
+            <p class="mb-4 text-xs tracking-wide text-gray-400">
+              Apple pie macaroon toffee jujubes pie tart cookie caramels.
+            </p>
+          </div>
+        </div>
+      </div>
+      
+    </div>
+  </div>
+
+  <div class="relative py-16 overflow-hidden bg-white">
+      <div class="relative px-4 mx-auto sm:px-6 lg:px-8 lg:max-w-3xl">
+
         <br />
         <h1
             class="mb-8 font-sans uppercase text-4xl font-bold tracking-tight text-black md:text-6xl text-left"
@@ -59,6 +109,7 @@ import { Component } from '@angular/core';
       </div>
     <app-team></app-team>
     </div>
+    
   `,
   styles: [
   ]
--- src/app/pages/venue.page.ts
+++ src/app/pages/venue.page.ts
@@ -21,7 +21,8 @@ export const routeMeta: RouteMeta = {
         'Midas Palace Hotel (Rome) - Located a few kilometers from the center of Rome Midas Palace Hotel four-star hotel and Convention Center is the combination of business and relaxation'
       "
     />
-    <app-page-image [image]="'https://res.cloudinary.com/dp1gwjz5w/image/upload/f_auto,q_auto/v1/venue/kouclpg2l8bkgbsbzkul'" />
+    <!-- <app-page-image [image]="'https://res.cloudinary.com/dp1gwjz5w/image/upload/f_auto,q_auto/v1/venue/kouclpg2l8bkgbsbzkul'" /> -->
+    <app-page-image [image]="'https://res.cloudinary.com/dp1gwjz5w/image/upload/c_pad,ar_16:9/v1710172530/midas-hotel-Main-Entrance-Midas_galt3a.jpg'" />
     <app-content />
   `,
   imports: [PageHeadComponent, PageImageComponent, ContentComponent],
