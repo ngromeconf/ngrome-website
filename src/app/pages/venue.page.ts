bd1164034dbc96b55eb6b211f0172731a00ca966
Gioio95 <63113360+GMalacaria@users.noreply.github.com>
2024-03-12 15:42:24 +0100

    fix: update img heade venue

--- src/app/pages/venue.page.ts
+++ src/app/pages/venue.page.ts
@@ -22,7 +22,7 @@ export const routeMeta: RouteMeta = {
       "
     />
     <!-- <app-page-image [image]="'https://res.cloudinary.com/dp1gwjz5w/image/upload/f_auto,q_auto/v1/venue/kouclpg2l8bkgbsbzkul'" /> -->
-    <app-page-image [image]="'https://res.cloudinary.com/dp1gwjz5w/image/upload/c_pad,ar_16:9/v1710172530/midas-hotel-Main-Entrance-Midas_galt3a.jpg'" />
+    <app-page-image [image]="'https://res.cloudinary.com/dp1gwjz5w/image/upload/c_pad,b_auto:predominant,fl_preserve_transparency/v1710172530/venue/midas-hotel-Main-Entrance-Midas_galt3a.jpg?_s=public-apps'" />
     <app-content />
   `,
   imports: [PageHeadComponent, PageImageComponent, ContentComponent],
