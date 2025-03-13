2ae40610051a36f538635a7407be303510735bcb
Luciano <luciano.murruni@gmail.com>
2025-03-13 05:37:05 +0100

    feat: new nav menu (#229)
    * feat: new nav menu
    
    * new nav & hero
    
    * update the ngrome shield

--- src/app/pages/index.page.ts
+++ src/app/pages/index.page.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit, Signal, inject } from '@angular/core';
-import { HeaderComponent } from '../components/layout/header/header.component';
 import { HeroComponent } from '../components/home/hero/hero.component';
-import { FeaturesComponent } from '../components/home/features/features.component';
 import { SponsorComponent } from '../components/sponsors/sponsor.component';
 import { Sponsors } from '../models/sponsor.model';
 import { CommonModule } from '@angular/common';
@@ -33,11 +31,11 @@ export const routeMeta: RouteMeta = {
     @if (sponsors$(); as Sponsors) {
       <app-sponsor [sponsors]="Sponsors.Main" [itemsPerRow]="1" type="Main" />
       <app-sponsor [sponsors]="Sponsors.Gold" [itemsPerRow]="3" type="Gold" />
-      <!-- <app-sponsor
+      <app-sponsor
         [sponsors]="Sponsors.Silver"
         [itemsPerRow]="3"
         type="Silver"
-      /> -->
+      />
       <!-- <app-sponsor
         [sponsors]="Sponsors.Bronze"
         [itemsPerRow]="5"
@@ -54,13 +52,16 @@ export const routeMeta: RouteMeta = {
         type="Technical"
       /> -->
       <app-sponsor [sponsors]="Sponsors.Event" [itemsPerRow]="1" type="Event" />
+      <app-sponsor
+        [sponsors]="Sponsors.Community"
+        [itemsPerRow]="5"
+        type="Community"
+      />
     }
   `,
   imports: [
     CommonModule,
-    HeaderComponent,
     HeroComponent,
-    FeaturesComponent,
     SponsorComponent,
     TicketsComponent,
     CallForPaperComponent,
