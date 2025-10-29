aacedf77b028dd3aaeb37a42a94953a033386903
lucianomurr <luciano.murruni@gmail.com>
2025-10-29 16:10:43 +0100

    feat: update cfp url

--- src/app/components/shared/constant.ts
+++ src/app/components/shared/constant.ts
@@ -1,4 +1,4 @@
-export const UrlCFP: string = 'https://forms.gle/9nToEntiHbg8WrPB8';
+export const UrlCFP: string = 'https://forms.gle/wvj1DqUYQVv16RDf9';
 
 export const CLASS_COLOR_BADGE: { [key: string]: string } = {
   red: 'bg-red-100 text-red-800 border-red-400',
